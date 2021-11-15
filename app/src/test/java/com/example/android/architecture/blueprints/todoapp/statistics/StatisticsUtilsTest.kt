494b35c45215de713c16e195d90c365a5d0f97ab
mooracle <yanuar.prakosa@gmail.com>
2021-11-15 14:27:04 +0700

    preparing additional tests for stats fragment

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -21,4 +21,28 @@ class StatisticsUtilsTest {
         assertThat(result.activeTasksPercent, `is`(100f))
         assertThat(result.completedTasksPercent, `is`(0f))
     }
+
+    @Test
+    fun getActiveAndCompletedStats_empty_returnZeros() {
+//        Test when the list is empty meaning not even completed task in it
+//        It should return zero percentage for both active task and completed task
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_noActive_returnsZeroHundred() {
+//        Test when the list have no active task (all done)
+//        It should return 0% to active task and 100% to completed task
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_error_null_returnsZeros() {
+//        Test when there are errors in the task List (in this case null errors)
+//        It should returns 0% for active task and completed task
+    }
+
+    @Test
+    fun  getActiveAndCompletedStats_both_returnsFortySixty() {
+//        Test when we have 5 tasks and 3 completed while 2 still active
+//        It should returns 40% to active tasks and 60% to completed tasks
+    }
 }
